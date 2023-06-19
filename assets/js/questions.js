cd77ae829c748227b3f1b65bbbb545e21a9264bb
Conor <chepenikconor@gmail.com>
2023-06-18 23:51:24 -0400

    putting the quiz in this repo

---
+++ assets/js/questions.js
@@ -0,0 +1,170 @@
+var questionArr = [
+    {
+      question: "What is Bitcoin?",
+      firstOption: "A decentralized digital currency",
+      secondOption: "A centralized digital currency",
+      thirdOption: "A physical form of currency",
+      fourthOption: "A type of stock",
+      correctAnswer: "#first-option"
+    },
+    {
+      question: "Who created Bitcoin?",
+      firstOption: "Satoshi Nakamoto",
+      secondOption: "Vitalik Buterin",
+      thirdOption: "Charlie Lee",
+      fourthOption: "Mark Zuckerberg",
+      correctAnswer: "#first-option"
+    },
+    {
+      question: "When was Bitcoin created?",
+      firstOption: "2005",
+      secondOption: "2008",
+      thirdOption: "2010",
+      fourthOption: "2013",
+      correctAnswer: "#second-option"
+    },
+    {
+      question: "What is the maximum supply of Bitcoin?",
+      firstOption: "10 million",
+      secondOption: "50 million",
+      thirdOption: "100 million",
+      fourthOption: "21 million",
+      correctAnswer: "#fourth-option"
+    },
+    {
+      question: "How are new Bitcoins created?",
+      firstOption: "They are printed by the government",
+      secondOption: "They are mined by solving complex mathematical problems",
+      thirdOption: "They are generated through a centralized banking system",
+      fourthOption: "They are exchanged for other cryptocurrencies",
+      correctAnswer: "#second-option"
+    },
+    {
+      question: "What is the underlying technology behind Bitcoin?",
+      firstOption: "Blockchain",
+      secondOption: "Artificial Intelligence",
+      thirdOption: "Virtual Reality",
+      fourthOption: "Quantum Computing",
+      correctAnswer: "#first-option"
+    },
+    {
+      question: "What is a Bitcoin wallet?",
+      firstOption: "A physical wallet to store Bitcoin notes",
+      secondOption: "A software program to store Bitcoin private keys",
+      thirdOption: "A bank account for Bitcoin",
+      fourthOption: "An online marketplace to buy Bitcoin",
+      correctAnswer: "#second-option"
+    },
+    {
+      question: "What is the process of verifying Bitcoin transactions called?",
+      firstOption: "Mining",
+      secondOption: "Trading",
+      thirdOption: "Staking",
+      fourthOption: "Lending",
+      correctAnswer: "#first-option"
+    },
+    {
+      question: "What is the first major purchase made with Bitcoin?",
+      firstOption: "A luxury car",
+      secondOption: "A pizza",
+      thirdOption: "A house",
+      fourthOption: "A yacht",
+      correctAnswer: "#second-option"
+    },
+    {
+      question: "What is the term for the smallest fraction of a Bitcoin?",
+      firstOption: "Satoshi",
+      secondOption: "Bit",
+      thirdOption: "Coin",
+      fourthOption: "Byte",
+      correctAnswer: "#first-option"
+    },
+    {
+      question: "Which country has the most Bitcoin miners?",
+      firstOption: "United States",
+      secondOption: "China",
+      thirdOption: "Russia",
+      fourthOption: "Japan",
+      correctAnswer: "#first-option"
+    },
+    {
+      question: "What is a 51% attack in the context of Bitcoin?",
+      firstOption: "A cyber attack on Bitcoin exchanges",
+      secondOption: "A majority of Bitcoin transactions being fraudulent",
+      thirdOption: "A single entity controlling more than half of the network's mining power",
+      fourthOption: "A government ban on Bitcoin",
+      correctAnswer: "#third-option"
+    },
+    {
+      question: "What is the purpose of Bitcoin mining?",
+      firstOption: "To burn coal and contribute to global warming",
+      secondOption: "To validate and secure transactions",
+      thirdOption: "To manipulate the price of Bitcoin",
+      fourthOption: "To support renewable energy initiatives",
+      correctAnswer: "#second-option"
+    },
+    {
+      question: "What is a private key in the context of Bitcoin?",
+      firstOption: "A secret code that allows access to Bitcoin funds",
+      secondOption: "A public address used to receive Bitcoin",
+      thirdOption: "A unique identifier for a Bitcoin transaction",
+      fourthOption: "A security feature to prevent double-spending",
+      correctAnswer: "#first-option"
+    },
+    {
+      question: "What is the role of a Bitcoin node?",
+      firstOption: "To mine new Bitcoins",
+      secondOption: "To spy on others",
+      thirdOption: "To verify and relay Bitcoin transactions",
+      fourthOption: "To facilitate peer-to-peer Bitcoin trading",
+      correctAnswer: "#third-option"
+    },
+    {
+      question: "What is the current block reward for successfully mining a Bitcoin block?",
+      firstOption: "1 Bitcoin",
+      secondOption: "5 Bitcoins",
+      thirdOption: "10 Bitcoins",
+      fourthOption: "25 Bitcoins",
+      correctAnswer: "#fourth-option"
+    },
+    {
+      question: "What is the pseudonymous nature of Bitcoin?",
+      firstOption: "Bitcoin transactions are completely anonymous",
+      secondOption: "Bitcoin transactions are traceable but not tied to real-world identities",
+      thirdOption: "Bitcoin transactions are public and linked to real-world identities",
+      fourthOption: "Bitcoin transactions can only be made by pseudonyms",
+      correctAnswer: "#second-option"
+    },
+    {
+      question: "What is a Bitcoin halving?",
+      firstOption: "A reduction in the block size of the Bitcoin blockchain",
+      secondOption: "A decrease in the mining difficulty of Bitcoin",
+      thirdOption: "A reduction in the reward given to Bitcoin miners",
+      fourthOption: "A split of the Bitcoin blockchain into two separate chains",
+      correctAnswer: "#third-option"
+    },
+    {
+      question: "What is a Bitcoin fork?",
+      firstOption: "A physical splitting of a Bitcoin into two separate coins",
+      secondOption: "A change in the underlying technology of Bitcoin",
+      thirdOption: "A software update that introduces new features to Bitcoin",
+      fourthOption: "A divergence in the blockchain resulting in two separate chains",
+      correctAnswer: "#fourth-option"
+    },
+    {
+      question: "What is the Lightning Network?",
+      firstOption: "A decentralized exchange for Bitcoin",
+      secondOption: "A layer-2 scaling solution for faster and cheaper Bitcoin transactions",
+      thirdOption: "A privacy-focused version of Bitcoin",
+      fourthOption: "A peer-to-peer lending platform for Bitcoin",
+      correctAnswer: "#second-option"
+    },
+    {
+      question: "What is the role of a Bitcoin full node?",
+      firstOption: "To mine new Bitcoins",
+      secondOption: "To store the entire history of Bitcoin transactions",
+      thirdOption: "To invalidate Bitcoin transactions",
+      fourthOption: "To create new rules for the Bitcoin network",
+      correctAnswer: "#second-option"
+    }
+  ];  
